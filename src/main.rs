7dfd8986ead505a8765c9c18cc3c7a2c3c3cb1f8
丁科 <me@dingkewz.com>
2021-03-28 22:06:40 +0800

    look ma, I can draw lines now!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,128 @@
+use anyhow::*;
+use futures::executor::block_on;
 use winit::{
     event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
+    window::{Window, WindowBuilder},
 };
+
+struct App {
+    instance: wgpu::Instance,
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc: wgpu::SwapChain,
+    pipeline: wgpu::RenderPipeline,
+}
+
+// 我想渲染一段直线
+// 我需要指定 vertex 以及它们的顺序
+// 在 wgpu 中如何指定
+impl App {
+    async fn new(window: &Window) -> Result<Self> {
+        let size = window.inner_size();
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    ..Default::default()
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+            label: None,
+            flags: wgpu::ShaderFlags::all(),
+            source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(include_str!(
+                "shader.wgsl"
+            ))),
+        });
+
+        let sc_format = adapter.get_swap_chain_preferred_format(&surface);
+        let sc = device.create_swap_chain(
+            &surface,
+            &wgpu::SwapChainDescriptor {
+                usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+                width: size.width,
+                height: size.height,
+                format: sc_format,
+                present_mode: wgpu::PresentMode::Fifo,
+            },
+        );
+
+        let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: None,
+            layout: None,
+            vertex: wgpu::VertexState {
+                module: &shader,
+                buffers: &[],
+                entry_point: "vs_main",
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[sc_format.into()],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::LineStrip,
+                ..Default::default()
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState::default(),
+        });
+
+        Ok(Self {
+            instance,
+            surface,
+            device,
+            queue,
+            sc,
+            pipeline,
+        })
+    }
+
+    pub fn render(&self) -> Result<()> {
+        let frame = self.sc.get_current_frame().expect("无法获取下一帧").output;
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        {
+            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: None,
+                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
+                        store: true,
+                    },
+                    resolve_target: None,
+                    attachment: &frame.view,
+                }],
+                depth_stencil_attachment: None,
+            });
+            rpass.set_pipeline(&self.pipeline);
+            rpass.draw(0..3, 0..1);
+        }
+
+        self.queue.submit(Some(encoder.finish()));
+        Ok(())
+    }
+}
 
 fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let app = block_on(App::new(&window)).unwrap();
 
     event_loop.run(move |event, _, control_flow| {
         // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
@@ -53,6 +169,7 @@ fn main() {
                 window.request_redraw();
             }
             Event::RedrawRequested(_) => {
+                app.render().unwrap();
                 // Redraw the application.
                 //
                 // It's preferable for applications that do not render continuously to render in
