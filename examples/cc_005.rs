29ab33e6ddb2113f4681ba116b9ef5fcb325a893
丁科 <me@dingkewz.com>
2021-07-04 21:43:53 +0800

    WIP: universe

--- examples/cc_005.rs
+++ examples/cc_005.rs
@@ -1,37 +1,36 @@
-/// Line
-/// 我们可以如何利用线条？
-///
-/// 单个线条有哪些性质?
-///
-/// 1. 长度
-/// 2. 粗细
-/// 3. 圆角弧度
-/// 4. 颜色
-/// 5. 方向
-///
-/// 两个线条有哪些性质
-///
-/// 1. 角度
+/// Star
 use nannou::prelude::*;
-
-struct Model {
-
-} 
 
 fn main() {
     nannou::app(model).update(update).run();
 }
 
+struct Model {}
+
 fn model(app: &App) -> Model {
-    app.new_window().size(1024, 1024).view(view);
+    app.new_window()
+        .size(1024, 1024)
+        .view(view)
+        .build()
+        .unwrap();
 
     Model {}
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {
-    
+    //
 }
 
-fn view(_app: &App, _model: &Model, _frame: Frame) {
-    
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(BLACK);
+    // draw.rect().x_y(0.0, 0.0).w_h(100.0, 100.0).color(PLUM);
+
+    draw.ellipse()
+        .radius(256.0)
+        .x_y(0.0, 0.0)
+        .no_fill()
+        .stroke_weight(0.3)
+        .stroke_color(WHITE);
+    draw.to_frame(app, &frame).unwrap();
 }
