4f27539bdee3ce58b4147005319885ea018b11bf
丁科 <me@dingkewz.com>
2021-07-05 21:40:33 +0800

    WIP: able to model a glowing border for the circle

--- examples/cc_005.rs
+++ examples/cc_005.rs
@@ -26,6 +26,22 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     draw.background().color(BLACK);
     // draw.rect().x_y(0.0, 0.0).w_h(100.0, 100.0).color(PLUM);
 
+    let num = 200;
+    let step = 2.0 * PI / (num as f32);
+    let half_length = 50.0;
+    let radius = 300.0;
+
+    for i in 0..num {
+        let r = random_range(0.0, 1.0);
+        let pt01 = pt2(radius, half_length * r);
+        let pt02 = pt2(radius, -half_length * r);
+
+        draw.line()
+            .rotate(i as f32 * step)
+            .points(pt01, pt02)
+            .color(WHITE)
+            .stroke_weight(0.2);
+    }
     draw.ellipse()
         .radius(256.0)
         .x_y(0.0, 0.0)
