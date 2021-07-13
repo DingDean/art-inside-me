627eade713ae5b8cb7e129f31a18cdbe9cc250ae
DingDean <me@dingkewz.com>
2021-07-13 19:49:36 +0800

    WIP: moon watching on the balcony

---
+++ examples/cc_007.rs
@@ -0,0 +1,14 @@
+/// 凭栏望月
+/// 2021-07-13 晚 7:30 下班后，在公司的阳台上看见了美好的落日余晖。
+/// 天色渐晚，露出天边的一弯月牙。
+/// 以此为念。
+
+use nannou::prelude::*;
+
+fn main() {
+    nannou::sketch(view).run();
+}
+
+fn view(_app: &App, _frame: Frame) {
+    // TODO:
+}
