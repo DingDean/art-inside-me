c9a2ee3ac61a7e05ca017b32d2486cf28b7df564
DingDean <me@dingkewz.com>
2021-10-02 16:41:29 +0800

    wip

---
+++ examples/cc_0010_particles.rs
@@ -0,0 +1,91 @@
+use nannou::noise::NoiseFn;
+use nannou::rand::*;
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model)
+        .update(update)
+        .run();
+}
+
+struct Particle {
+    pos: Vector2,
+    vel: Vector2,
+}
+
+impl Particle {
+    fn new(x:f32, y:f32) -> Particle {
+        Particle {
+            pos: vec2(x,y),
+            vel: vec2(0.,0.),
+        }
+    }
+
+    fn update(&mut self, dir: Vector2) {
+        self.pos += self.vel;
+        self.vel += dir;
+        self.vel *= 0.8;
+    }
+}
+
+
+struct Model {
+    particles: Vec<Particle>
+}
+
+fn model(app: &App) -> Model {
+    app.new_window().size(600,600).view(view).build().unwrap();
+
+    let r = app.window_rect().right() as f32;
+    let l = app.window_rect().left() as f32;
+
+    let w = l - r;
+    let t = app.window_rect().top() as f32;
+    let b = app.window_rect().bottom() as f32;
+
+    let h = t - b;
+
+    let mut p = vec![];
+    for _i in 0..2000 {
+        let x = random_f32() * w + r;
+        let y = random_f32() * h + b;
+        p.push(Particle::new(x,y));
+    }
+
+    Model{
+        particles: p
+    }
+}
+
+fn update(app: &App, model: &mut Model, _update:Update) {
+    let noise = nannou::noise::Perlin::new();
+    let t = app.elapsed_frames() as f64/ 100.;
+    for i in 0..model.particles.len() {
+        let p = &mut model.particles[i];
+        let x = noise.get([ p.pos.x as f64 / 128., p.pos.y as f64 / 137., t + i as f64/ 1000. ]);
+        let y = noise.get([ -p.pos.y as f64 / 128., p.pos.x as f64 / 137., t + i as f64/ 1000. ]);
+
+        let a = vec2(x as f32, y as f32);
+        p.update(a);
+    }
+}
+
+fn view( app: &App, model: &Model, frame: Frame){
+    let draw = app.draw();
+    for p in &model.particles {
+        draw.ellipse().xy(p.pos).w_h(1.0,1.0).color(hsla(0.1,1.,5.,0.01));
+    }
+    draw.to_frame(app, &frame).unwrap();
+    if frame.nth() < 500 {
+        let file_path = captured_frame_path(app, &frame);
+        app.main_window().capture_frame(file_path);
+    }
+}
+
+fn captured_frame_path(app: &App, frame: &Frame) -> std::path::PathBuf {
+    app.project_path()
+        .expect("failed to locate `project_path`")
+        .join("frames")
+        .join(format!("{:04}", frame.nth()))
+        .with_extension("png")
+}
--- examples/cc_009_bytedance_new.rs
+++ examples/cc_009_bytedance_new.rs
@@ -1,41 +1,62 @@
+// use nannou::prelude::*;
+
+// fn main() {
+//     nannou::app(model).update(update).run();
+// }
+
+// struct Model {}
+
+// /// 一个 Byte 可以是 8 个一字排开的点
+// /// 每个值为 1 的点用半圆相连
+// struct Byte {
+//     num: i8,
+// }
+
+// impl Byte {
+//     pub fn new() -> Self {
+//         Self { num: 0 }
+//     }
+
+//     fn dance(&self, draw: &Draw) {
+//         draw.polyline
+//     }
+// }
+
+// fn model(app: &App) -> Model {
+//     app.new_window()
+//         .size(1024, 1024)
+//         .view(view)
+//         .build()
+//         .unwrap();
+
+//     Model {}
+// }
+
+// fn update(_app: &App, _model: &mut Model, _update: Update) {
+//     //
+// }
+
+// fn view(app: &App, _model: &Model, frame: Frame) {
+//     let draw = app.draw();
+//     draw.background().color(WHITE);
+//     draw.rect().x_y(0.0, 0.0).w_h(100.0, 100.0).color(PLUM);
+
+//     let byte = Byte::new();
+
+//     byte.dance(&draw);
+
+//     draw.to_frame(app, &frame).unwrap();
+// }
 use nannou::prelude::*;
 
-fn main() {
-    nannou::app(model).update(update).run();
+pub fn main() {
+    nannou::sketch(view).run();
 }
 
-struct Model {}
-
-struct Byte {}
-
-impl Byte {
-    fn dance(&self, draw: &Draw) {
-        // TODO
-    }
-}
-
-fn model(app: &App) -> Model {
-    app.new_window()
-        .size(1024, 1024)
-        .view(view)
-        .build()
-        .unwrap();
-
-    Model {}
-}
-
-fn update(_app: &App, _model: &mut Model, _update: Update) {
-    //
-}
-
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn view(app: &App, frame: Frame) {
     let draw = app.draw();
+
     draw.background().color(WHITE);
-    draw.rect().x_y(0.0, 0.0).w_h(100.0, 100.0).color(PLUM);
-
-    let byte = Byte {};
-
-    byte.dance(&draw);
 
     draw.to_frame(app, &frame).unwrap();
 }
